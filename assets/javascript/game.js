979bcea5b7df1d6edf220e7c0622e4cb9ee878bf
Green Vine <barryaervin@gmail.com>
2019-10-23 10:52:26 -0400

    made attack, defense, and counter functions work.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,9 @@ const characters = [
         name: 'Darth Sideous',
         hp: 100,
         attack: 40,
-        counter: 15,
-        defense: 20,
-        attackIncrementer: 2,
+        counter: 30,
+        defense: 15,
+        attackIncrementer: 1,
         sith: true,
         image: "assets/images/characters/sideous.png",
         quote: "Release your anger",
@@ -16,8 +16,8 @@ const characters = [
         name: 'Jar Jar Binks Sith Lord',
         hp: 150,
         attack: 30,
-        counter: 10,
-        defense: 15,
+        counter: 20,
+        defense: 20,
         attackIncrementer: 3,
         sith: true,
         image: "assets/images/characters/jar.png",
@@ -27,10 +27,10 @@ const characters = [
     {
         name: 'Darth Vader',
         hp: 200,
-        attack: 30,
-        counter: 20,
-        defense: 20,
-        attackIncrementer: 3,
+        attack: 35,
+        counter: 25,
+        defense: 25,
+        attackIncrementer: 1,
         sith: true,
         image: "assets/images/characters/vader.png",
         quote: "Nooooooooooooo",
@@ -52,9 +52,9 @@ const characters = [
         name: 'Yoda',
         hp: 100,
         attack: 20,
-        counter: 30,
+        counter: 40,
         defense: 15,
-        attackIncrementer: 5,
+        attackIncrementer: 4,
         sith: false,
         image: "assets/images/characters/yoda.png",
         quote: "Patience you must have padawan",
@@ -66,7 +66,7 @@ const characters = [
         attack: 25,
         counter: 30,
         defense: 20,
-        attackIncrementer: 4,
+        attackIncrementer: 5,
         sith: false,
         image: "assets/images/characters/luke.png",
         quote: "I will not fight you",
@@ -78,20 +78,25 @@ const toolbarText = [`
                     <p>Choose a player from the Jedi or Sith.</p>
                     <p>You will then
                     fight the three members from the other side in the order you
-                    choose. Your enemies cannot defend but they can counterattack.
+                    choose. Your enemies cannot defend but they will counterattack.
                     your attack will go up each time you attack as well. Good Luck!</p>
                 </div>`,
     `<div class="column instructions">
                     <h2>Instructions:</h2>
                     <p>You will have to defeat all three enemies to win!
-                        Choose your first opponent wisely.</p>
+                        Click to choose your next opponent.</p>
                 </div>`,
     `<div class="column instructions">
                     <h2>Instructions</h2>
-                    <p>Click attack to strike. Your attack will go up each time.
+                    <p>Click attack to strike until their HP = 0. Your attack will go up each time you attack.
                     Your enemy will counterattack automatically.</p>
                 </div>
+`, `<div class="column instructions">
+                    <h2>You Win!</h2>
+                    <p>Your enemies have been vanquished and the force is back in balance.</p>
+                </div>
 `
+
 ];
 let gameState = null;
 let playerChoice = null;
@@ -102,16 +107,11 @@ let enemiesToDefeat = []
 // look into switch statements
 // look into truthy and falsey
 //  Things left to do:
-//      -finish attack function
-//          -when enemy dies add remaining enemies
-//              -when none remain, win.
-//      -setup counterattack function 
-//          -check against playerChoice.defense
-//          -subtract from playerChoice.hp
-//      -create
-//
-//
-//
+//     -Lose condition
+//        - attack incrementer?
+//     -balancing stats
+//     -styling
+
 function main() {
     let darkSide = document.getElementsByClassName("dark-side")[0];
     let lightSide = document.getElementsByClassName("light-side")[0];
@@ -135,7 +135,7 @@ function main() {
                 darkSide.innerHTML = '';
                 darkSide.appendChild(card);
                 for (let i = 0; i < characters.length; i++) {
-                    if (characters[i].sith === false) {
+                    if (characters[i].sith === false && characters[i].hp > 0) {
                         enemiesToDefeat.push(characters[i]);
                     }
                 }
@@ -144,35 +144,47 @@ function main() {
                 lightSide.innerHTML = '';
                 lightSide.appendChild(card);
                 for (let i = 0; i < characters.length; i++) {
-                    if (characters[i].sith === true) {
+                    if (characters[i].sith === true && characters[i].hp > 0) {
                         enemiesToDefeat.push(characters[i]);
                     }
                 }
             }
 
         } else if (gameState === "attack mode") {
-            let card = createCharacterCard(enemyChoice);
             if (enemyChoice.sith === true) {
+                let card = createCharacterCard(enemyChoice);
+                let playerCard = createCharacterCard(playerChoice);
                 darkSide.innerHTML = '';
-                darkSide.appendChild(card);
-            } else {
                 lightSide.innerHTML = '';
+                darkSide.appendChild(card);
+                lightSide.appendChild(playerCard);
+            } else {
+                let card = createCharacterCard(enemyChoice);
+                let playerCard = createCharacterCard(playerChoice);
+                lightSide.innerHTML = '';
+                darkSide.innerHTML = '';
                 lightSide.appendChild(card);
+                darkSide.appendChild(playerCard);
             }
 
         }
 
     }
 
-    //when enemyChoice.hp === 0, clear html,
-    //add remaining two characters back into darkSide or lightSide field.
     function attack() {
         enemyChoice.hp -= playerChoice.attack;
         if (enemyChoice.hp > 0) {
-            characterRender()
+            playerChoice.hp -= (enemyChoice.counter - playerChoice.defense);
+            if (playerChoice.hp <= 0) {
+                alert("You lose! The force is out of balance.");
+            } else {
+                playerChoice.attack += playerChoice.attackIncrementer;
+                characterRender();
+                toolbarRender();
+            }
         } else {
             enemiesDefeated += 1;
-            if (enemiesDefeated === 2) {
+            if (enemiesDefeated === 3) {
                 darkSide.innerHTML = '';
                 lightSide.innerHTML = '';
                 let resetButton = document.createElement('button');
@@ -180,7 +192,7 @@ function main() {
                 resetButton.innerHTML = 'Play again?';
                 let toolbarContent = document.createElement("div");
                 toolbarContent.className = "toolbar-content";
-                toolbarContent.innerHTML = toolbarText[0];
+                toolbarContent.innerHTML = toolbarText[3];
                 toolbar.innerHTML = '';
                 toolbar.appendChild(toolbarContent);
                 toolbar.appendChild(resetButton);
@@ -191,27 +203,25 @@ function main() {
                 enemiesToDefeat.splice(enemiesToDefeat.indexOf(enemyChoice), 1);
                 enemyChoice = null;
                 gameState = "choose enemy";
+                toolbarRender();
                 if (playerChoice.sith === true) {
                     lightSide.innerHTML = '';
+                    for (let i = 0; i < enemiesToDefeat.length; i++) {
+                        let card = createCharacterCard(enemiesToDefeat[i]);
+                        lightSide.appendChild(card);
+                    }
+
                 } else {
                     darkSide.innerHTML = '';
-                }
-
-                for (let i = 0; i < enemiesToDefeat.length; i++) {
-                    let card = createCharacterCard(enemiesToDefeat[i]);
-                    if (enemiesToDefeat[i].sith === true) {
+                    for (let i = 0; i < enemiesToDefeat.length; i++) {
+                        let card = createCharacterCard(enemiesToDefeat[i]);
                         darkSide.appendChild(card);
-                    } else {
-                        lightSide.appendChild(card);
                     }
                 }
             }
         }
-        if (enemiesDefeated < 2) {
-            characterRender();
-        }
-    }
 
+    }
 
     function toolbarRender() {
         let resetButton = document.createElement('button');
@@ -260,10 +270,10 @@ function main() {
         <img class="card-img" src="${character.image}">
             <div class="card-descr">
                 <p class="name">${character.name}</p>
-                <p class="hp">HP:${character.hp}</p>
-                <p class="attack">Attack:${character.attack}</p>
-                <p class="counter">Counter:${character.counter}</p>
-                <p class="defense">Defense:${character.defense}</p>
+                <p class="hp">HP: ${character.hp}</p>
+                <p class="attack">Attack: ${character.attack}</p>
+                <p class="counter">Counter: ${character.counter}</p>
+                <p class="defense">Defense: ${character.defense}</p>
                 <p class="flavor">${character.quote}</p>
             </div>`;
         card.addEventListener("click", function () {
@@ -286,18 +296,4 @@ main();
 
 
 
-//look up truthy and falsey//
-//set up a conditonal sequence in main where it starts with all characters
 
-
-//when attack button is pressed:
-//substract (pl - enemyDef) from enemy HP
-    //if enemyHp=0 
-        //bring other two enemies on the screen
-           //choose next enemy
-            //on click get rid of other enemy and center new enemy
-              //return to attack mode
-// else if enemy counters, substract their from player hp
-    //if player HP = 0, Game Over
-        //Try again? (reset)
-//add 5 to player attack stat.
